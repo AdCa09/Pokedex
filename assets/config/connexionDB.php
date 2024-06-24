e3f123392baf2f28f2d3b1597b8f86b4bf94b29d
AdCa09 <caufriezadrien0@gmail.com>
2024-06-24 13:50:26 +0200

    db fichier + start admin board

---
+++ assets/config/connexionDB.php
@@ -0,0 +1,15 @@
+<?php
+
+$user = 'adri'; // Remplacer par l'input du login
+$psw = 'adri';
+$host = 'localhost';
+$dbname = 'pokedex'; 
+
+try {
+    
+    $dbh = new PDO("mysql:host=$host;dbname=$dbname", $user, $psw); 
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo 'Connexion établie';
+} catch (PDOException $e) {
+    echo 'Connexion impossible : ' . $e->getMessage();
+}
