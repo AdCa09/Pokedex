771f803d299736d90830e81f75b5b0715dbd9a97
AdCa09 <caufriezadrien0@gmail.com>
2024-06-24 17:02:43 +0200

    login pour admin, fonctionne à moitier ;(

--- assets/config/connexionDB.php
+++ assets/config/connexionDB.php
@@ -10,6 +10,7 @@ try {
     $dbh = new PDO("mysql:host=$host;dbname=$dbname", $user, $psw); 
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo 'Connexion établie';
+    
 } catch (PDOException $e) {
     echo 'Connexion impossible : ' . $e->getMessage();
 }
