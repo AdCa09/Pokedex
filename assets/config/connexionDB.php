a2bff3dd6221f4d7caf56fe5c72e83bc00743b70
ludovic lacroix <ludovic.lacroix82@gmail.com>
2024-06-25 09:33:40 +0200

    htaccess

--- assets/config/connexionDB.php
+++ assets/config/connexionDB.php
@@ -9,12 +9,8 @@ try {
     
     $dbh = new PDO("mysql:host=$host;dbname=$dbname", $user, $psw); 
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-<<<<<<< HEAD
     echo 'Connexion établie';
     
 } catch (PDOException $e) {
-=======
-    } catch (PDOException $e) {
->>>>>>> ludovic
     echo 'Connexion impossible : ' . $e->getMessage();
 }
